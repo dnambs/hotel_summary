0461cd6aa67350305a7cca9f57aa236084b80e6f
Dev N <devpaduthol@gmail.com>
2021-12-10 01:10:54 -0600

    updated

--- hotel_search_streamlit.py
+++ hotel_search_streamlit.py
@@ -1,48 +1,41 @@
-
-import re
-import pickle as pkl
 import pandas as pd
-import scipy.spatial
 import torch
 import streamlit as st
 from sentence_transformers import SentenceTransformer, util
-#from summarizer import Summarizer
-from summarizer.sbert import SBertSummarizer
-from tqdm import tqdm
 from wordcloud import WordCloud
+from PIL import Image
+
 import matplotlib.pyplot as plt
 
 st.header("London Hotels")
-
-
-
+image = Image.open('londonpic.jpg', )
+st.image(image, caption='By Eva Dang')
 
 
 def main():
     corpus = pd.read_pickle('corpus.pkl')
     corpus_embeddings = pd.read_pickle('corpus_embeddings.pkl')
     df = pd.read_pickle('df.pkl')
-    query = st.text_input('Enter your query here:')
+    st.subheader('Enter the specifications for a hotel below:')
+    query = st.text_input('')
     embedder = SentenceTransformer('all-MiniLM-L6-v2')
     query_embedding = embedder.encode(query, convert_to_tensor=True)
 
     if st.button('Search'):
         closest_n = 3
-        st.write("Top 3 most similar sentences in corpus:")
-        #distances = scipy.spatial.distance.cdist([query_embedding], corpus_embeddings, "cosine")[0]
+        st.markdown("<b>Here are the top 3 hotels that match your search:</b>", unsafe_allow_html=True)
+        # distances = scipy.spatial.distance.cdist([query_embedding], corpus_embeddings, "cosine")[0]
         distances = util.pytorch_cos_sim(query_embedding, corpus_embeddings)[0]
         top_results = torch.topk(distances, k=closest_n)
 
-        #results = zip(range(len(distances)), distances)
-        #results = sorted(results, key=lambda x: x[1])
-
+        # results = zip(range(len(distances)), distances)
+        # results = sorted(results, key=lambda x: x[1])
 
         for score, idx in zip(top_results[0], top_results[1]):
-
             row_dict = df.loc[df['all_review'] == corpus[idx]]['hotelName'].values[0]
-            st.markdown("<b>Hotel name:  </b>" + row_dict,unsafe_allow_html=True)
+            st.markdown("<b>Hotel name:  </b>" + row_dict, unsafe_allow_html=True)
             summary_dict = df.loc[df['all_review'] == corpus[idx]]['summary'].values[0]
-            st.markdown("<b>Summary: </b><p>"+ summary_dict +"</p>", unsafe_allow_html=True)
+            st.markdown("<b>Summary: </b><p> " + summary_dict + "</p>", unsafe_allow_html=True)
 
             wordcloud = WordCloud(max_font_size=50, max_words=100, background_color="black").generate(corpus[idx])
 
@@ -54,7 +47,6 @@ def main():
             st.pyplot()
 
             st.write("-------------------------------------------")
-
 
 
 if __name__ == '__main__':
